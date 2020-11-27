abb140443333fb4f5729e92491b3c053307a84e6
saumit10 <74811431+saumit10@users.noreply.github.com>
2020-11-27 17:41:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sklearn.preprocessing import MinMaxScaler
 app = Flask(__name__)
 
 port = int(os.environ.get('PORT', 5000))
-app.run(debug=True, host='0.0.0.0', port=port)
+
 
 # Load Models
 iris = pickle.load(open('templates/Supervised/Classification/Iris_Classifier/iris.pkl', 'rb'))
